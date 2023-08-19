63b671aeecb78fe7add63e261e11d77bcb4c5401
forpromo <142648492+forpromo@users.noreply.github.com>
2023-08-19 10:38:12 +0530

    Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}\n𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
 
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/cinesandhadhii>CINE</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/AdityaxTG>ADITYA</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : 𝙼𝙾𝙽𝙶𝙾-𝙳𝙱
@@ -10,11 +10,11 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚅𝙴𝚁𝚂𝙸𝙾𝙽: 𝚟3.0.0"""
 
     SOURCE_TXT = """<b>NOTE:</b>
-- 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁 👉 :<a href=https://t.me/+bTbibXWES_pmZGI1>HARSHA</a>
+- 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁 👉 :<a href=https://t.me/TrollerMawa_Ofcl>TrollerMawa</a>
 
 <b>DEVS:</b>
-- 𝙳𝚎𝚟 <a href=https://t.me/+bTbibXWES_pmZGI1>CINE</a>
-- 𝙳𝚎𝚟 <a href=https://t.me/+bTbibXWES_pmZGI1>CINE</a>"""
+- 𝙳𝚎𝚟 <a href=https://t.me/AdityaxTG>ADITYA</a>
+- 𝙳𝚎𝚟 <a href=https://t.me/TrollerMawa_Ofcl>TROLLER MAWA</a>"""
 
     FILE_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝐅𝐢𝐥𝐞 𝐒𝐭𝐨𝐫𝐞 𝐌𝐨𝐝𝐮𝐥𝐞../
 
@@ -28,9 +28,9 @@ class script(object):
 
 ⪼ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞 ›
 
-<code>/batch https://t.me/+bTbibXWES_pmZGI1 https://t.me/+bTbibXWES_pmZGI1</code>
+<code>/batch https://t.me/TrollerMawa_Ofcl https://t.me/TrollerMawa_Ofcl</code>
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/+bTbibXWES_pmZGI1><b>CINE</b></a>"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/AdityaxTG><b>ADITYA</b></a>"""
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
@@ -188,7 +188,7 @@ By @{} """
 
 ➪ 𝖩𝗎𝗌𝗍 𝗌𝖾𝗇𝖽 𝗆𝖾 𝖺 𝗂𝗆𝖺𝗀𝖾 𝗍𝗈 𝖾𝖽𝗂𝗍 ✨
 
-𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/+bTbibXWES_pmZGI1>HARSHA</a>"""
+𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/TrollerMawa_Ofcl>ADITYA</a>"""
 
     RESTRIC_TXT = """➤ 𝐇𝐞𝐥𝐩: Mᴜᴛᴇ 🚫
 
